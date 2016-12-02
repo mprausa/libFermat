1db6b23b34c86485c29e79bf47e969844ee8547f
Mario Prausa <prausa@physik.rwth-aachen.de>
2016-12-03 00:11:35 +0100

    license statement attached.

--- tool/fermat.cpp
+++ tool/fermat.cpp
@@ -1,5 +1,24 @@
 // vim: set expandtab shiftwidth=4 tabstop=4:
 
+/*
+ *  tool/fermat.cpp
+ *
+ *  Copyright (C) 2016 Mario Prausa
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as 
+ *  published by the Free Software Foundation, either version 3 of the 
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <FermatArray.h>
 #include <string>
 #include <iostream>
